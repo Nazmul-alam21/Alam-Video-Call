ba9a2dd10f40ddd2253cf37f6097f030d5ff2708
Nazmul alam <alamnazmul3352@gmail.com>
2025-03-21 10:30:07 +0530

    DEPLOYING FRONTEND

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import httpStatus from "http-status";
+import server from "../environment";
 
 
 
@@ -9,7 +10,7 @@ import httpStatus from "http-status";
 export const  AuthContext = createContext({});
 
 const client = axios.create({
-    baseURL: "http://localhost:8000/api/v1/users"
+    baseURL: `${server}/api/v1/users`
 })
 
 export const AuthProvider = ({children}) => {
